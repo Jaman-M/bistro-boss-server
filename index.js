d5b406d78c6b4a633c4a7df3283067e753e0627f
Jaman-M <jaman.manik1@gmail.comm>
2023-11-25 11:19:41 +0800

    delete cart item

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // connect with mongoDB driver
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mkg81.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -63,6 +63,15 @@ async function run() {
             res.send(result);
 
         });
+
+        //delete cart item
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
         // Send a ping to confirm a successful connection
