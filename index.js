bf58997dc50eab0a44d25a9330d22bd44e36e1de
Jaman-M <jaman.manik1@gmail.comm>
2023-11-29 14:42:23 +0800

    added users api , and admin api

--- index.js
+++ index.js
@@ -34,12 +34,41 @@ async function run() {
         const cartCollection = client.db('bistroDb').collection("carts");
 
         //users related apis 
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
+            // console.log(user);
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+            // console.log('existing user', existingUser);
+            if (existingUser) {
+                return res.send({ message: 'User already exists' })
+            }
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
- 
+
+        // for admin role
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = {_id: new ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+
+        })
+
 
         //menu related api
         app.get('/menu', async (req, res) => {
